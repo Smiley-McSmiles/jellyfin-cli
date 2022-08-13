50ee2d05ce1a153c06941b07c481090c4868b4a2
Smiley McSmiles <31331596+Smiley-McSmiles@users.noreply.github.com>
2022-08-13 13:21:48 -0500

    Add files via upload

--- .github/version_change.sh
+++ .github/version_change.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo "Please enter the current version to change"
-echo "EXAMPLE: 'v1.3.7'"
-read currentVersion
+currentVersion=$(cat ../jellyfin.1 | grep " - v" | cut -d "v" -f2)
+echo "Current version is v$currentVersion"
 echo
 echo "Please enter the new version number"
-echo "EXAMPLE: 'v1.4.0'"
+echo "EXAMPLE: '1.4.0'"
 read newVersion
 
-sed -i -e "s|$currentVersion|$newVersion|g" ../README.md
-sed -i -e "s|$currentVersion|$newVersion|g" ../jellyfin.1
-sed -i -e "s|$currentVersion|$newVersion|g" ../scripts/jellyfin
+sed -i -e "s|v$currentVersion|v$newVersion|g" ../README.md
+sed -i -e "s|v$currentVersion|v$newVersion|g" ../jellyfin.1
+sed -i -e "s|v$currentVersion|v$newVersion|g" ../scripts/jellyfin
